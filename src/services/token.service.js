f95b9bbea4a0336bf406ffedf5ef22e8e7aec714
Eniola Osabiya <eosabiya@gmail.com>
2021-11-08 13:44:08 +0100

    add userCodeName to JwtToken

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -4,7 +4,7 @@ const JWT = require("jsonwebtoken");
 class TokenService {
     // Create a new Token for a user
     async generateToken(user) {
-        const token = JWT.sign({ id: user._id }, JWT_SECRET);
+        const token = JWT.sign({ id: user._id, codeName: user.codeName }, JWT_SECRET);
         return token;
     }
 }
