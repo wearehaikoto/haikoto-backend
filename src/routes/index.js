d11b416c12133ccdfe43df5576fe691cf3d5b156
Eniola Osabiya <eosabiya@gmail.com>
2021-11-12 22:25:45 +0100

    trimIncomingRequests middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
+const trimIncomingRequests = require("../middlewares/trim-incoming-requests.middleware");
+
+// Trim all incoming requests
+router.use(trimIncomingRequests);
 
 router.use("/auth", require("./auth.route.js"));
 
