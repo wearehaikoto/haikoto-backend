238be150571a758e29906c76f7adcdc31bd61a75
Eniola Osabiya <eosabiya@gmail.com>
2021-11-30 04:39:15 +0100

    added isDeleted flag for user Model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema(
             enum: ["user", "admin"],
             default: "user"
         },
+        isDeleted: {
+            type: Boolean,
+            default: false
+        },
     },
     {
         timestamps: true
