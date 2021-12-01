c3a4e3c701fff3e02e1887fea01fc0837cf5bb87
Eniola Osabiya <eosabiya@gmail.com>
2021-12-01 22:18:25 +0100

    reset playground

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,39 +17,8 @@ router.use("/game", require("./game.route.js"));
 
 // Allow Playground to be accessed from localhost
 if (process.env.NODE_ENV !== "production") {
-  const CardModel = require("../models/card.model");
   router.use("/playground", async (req, res) => {
-    const cards = await CardModel.find({});
-    res.json(cards);
-
-
-
-    // - todo: run when switched to ATLAS
-
-
-    // const allCardsHashtags = [];
-    // cards.forEach(card => card.cardHashtags.forEach(hashtag => allCardsHashtags.push(hashtag)));
-    // // For reach #hashtags, create a new card for it
-    // allCardsHashtags.forEach(async (hashtag) => {
-    //   // Create a new card
-    //   const newCard = new CardModel({
-    //     userId: "",
-    //     cardTitle: hashtag,
-    //     cardImage: "https://i.ibb.co/0FtrsWH/green-screen.png",
-    //     cardHashtags: [hashtag],
-    //     cardType: "parent"
-    //   });
-
-    //   // Save the new card
-    //   await newCard.save();
-    // });
-
-    // return res.json({ allCardsHashtags });
-
-
-    // Add a cardType child to all cards 
-    // const cards = await CardModel.updateMany({}, { $set: { cardType: "child" } });
-    // return res.json({ cards });
+    res.json({});
   });
 }
 
